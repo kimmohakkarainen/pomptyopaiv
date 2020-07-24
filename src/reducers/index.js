e2e10b9644356d3be806c751d2e7f6c160fa19ee
kimmohakkarainen <30873934+kimmohakkarainen@users.noreply.github.com>
2020-07-24 13:27:45 +0300

    working poistettu

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,32 +1,10 @@
 export default function pompState(
   state = {
     calendar: [],
-    working: {
-      Katulenkki: 0,
-      Metsalenkki: 0,
-      OhituksetLenkilla: 0,
-      Juoruaminen: [],
-      LenkinArvio: [],
-      Rauhoittuminen: 0,
-      NamietsintaSisalla: 0,
-      NamietsintaUlkona: 0,
-      Erottelu: [],
-      Ilmaisu: [],
-      Tekija: [],
-      Kontakti: [],
-      Arvio: [],
-      Alytehtava: 0,
-      OhituksiaSosiaalistamisessa: 0,
-      SosiaalistamisenKesto: 0,
-      Sosiaalistettavat: [],
-      SosiaalistamisenArvio: [],
-      UlkoiluLopina: "",
-      TyoskentelyLopina: "",
-      SosiaalistamisLopina: ""
-    },
     metsalenkit: [],
     katulenkit: [],
-    merkinnat: []
+    merkinnat: [],
+    summary: []
   },
   action
 ) {
